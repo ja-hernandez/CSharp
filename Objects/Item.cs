e4a02ef262fbe766bcb3bf884fc62756cf1234d0
Ben Clark <brclark15@gmail.com>
2021-01-13 11:20:09 -0600

    Inheritance starter

--- Objects/Item.cs
+++ Objects/Item.cs
@@ -4,20 +4,16 @@ using System.Text;
 
 namespace Classes
 {
-/*    class Item
+    public class Item
     {
-        *//* 
-        As you can see you have existing code to make a shop run. Take a look at that code because your job is going to be to write a class that will work with that code. 
+        public string Name { get; set; }
 
-        ** this one is a little more complex than the others, but your Hints folder will only have some hints as to what's going on with the already established code, not what you need to do.**
+        public bool Portable { get; set; }
 
+        public bool Usable { get; set; }
+        public int NumUses { get; set; }
 
+        public int Space { get; set; }
+    }
 
-        You will need to make properties that meet the requirements for that shop, as well as building a constructor to make sure those properties are adequately populated.
-
-
-        Then you'll need to make some objects inside your program class. 
-
-         *//*
-    }*/
 }
